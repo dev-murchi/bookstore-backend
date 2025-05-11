497d6fd07c233be741e48cf2eaf6c76472b439a0
Murat ÇAY <murtcay@gmail.com>
2025-05-11 18:12:39 +0300

    add order-refund-created, order-refund-completed, and order-refund-failed jobs to the mailSenderQueue, and implement corresponding handler methods in the MailSenderQueueProcessor

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,10 +1,18 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Queue } from 'bullmq';
+import {
+  OrderRefundJob,
+  OrderStatusUpdateJob,
+  PasswordResetJob,
+} from '../queue-processor/mail-sender-queue.processor';
 
 @Injectable()
 export class EmailService {
   constructor(
-    @Inject('MailSenderQueue') private readonly mailSenderQueue: Queue,
+    @Inject('MailSenderQueue')
+    private readonly mailSenderQueue: Queue<
+      OrderStatusUpdateJob | PasswordResetJob | OrderRefundJob
+    >,
   ) {}
 
   async sendOrderStatusUpdate(orderId: string, status: string, email: string) {
@@ -38,6 +46,44 @@ export class EmailService {
       throw new Error(
         'Unable to send password reset email at this time. Please try again later.',
       );
+    }
+  }
+
+  async sendRefundCreatedMail(data: {
+    orderId: string;
+    amount: string;
+    email: string;
+    customerName: string;
+  }) {
+    try {
+      await this.mailSenderQueue.add('order-refund-created', data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async sendRefundCompleteddMail(data: {
+    orderId: string;
+    amount: string;
+    email: string;
+    customerName: string;
+  }) {
+    try {
+      await this.mailSenderQueue.add('order-refund-completed', data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async sendRefundFailedMail(data: {
+    orderId: string;
+    amount: string;
+    email: string;
+    customerName: string;
+    failureReason: string;
+  }) {
+    try {
+      await this.mailSenderQueue.add('order-refund-failed', data);
+    } catch (error) {
+      console.error(error);
     }
   }
 }
--- src/queue-processor/mail-sender-queue.processor.ts
+++ src/queue-processor/mail-sender-queue.processor.ts
@@ -1,7 +1,25 @@
 import { Processor, WorkerHost } from '@nestjs/bullmq';
 import { Job } from 'bullmq';
 import { MailSenderService } from 'src/mail-sender/mail-sender.service';
+export interface OrderStatusUpdateJob {
+  orderId: string;
+  status: string;
+  email: string;
+}
 
+export interface PasswordResetJob {
+  email: string;
+  username: string;
+  link: string;
+}
+
+export interface OrderRefundJob {
+  orderId: string;
+  amount: string;
+  email: string;
+  customerName: string;
+  failureReason?: string;
+}
 @Processor('mail-sender-queue')
 export class MailSenderQueueProcessor extends WorkerHost {
   constructor(private readonly mailSenderService: MailSenderService) {
@@ -10,10 +28,19 @@ export class MailSenderQueueProcessor extends WorkerHost {
   async process(job: Job): Promise<any> {
     switch (job.name) {
       case 'order-status-mail':
-        await this.sendOrderStatusMail();
+        await this.sendOrderStatusMail(job.data);
         break;
       case 'password-reset':
-        await this.passwordReset(job);
+        await this.passwordReset(job.data);
+        break;
+      case 'order-refund-created':
+        await this.sendRefundCreatedMail(job.data);
+        break;
+      case 'order-refund-completed':
+        await this.sendRefundCompletedMail(job.data);
+        break;
+      case 'order-refund-failed':
+        this.sendRefundFailedMail(job.data);
         break;
       default:
         console.log(job.name, job.data);
@@ -22,13 +49,55 @@ export class MailSenderQueueProcessor extends WorkerHost {
     return Promise.resolve({ success: true });
   }
 
-  private async sendOrderStatusMail() {
+  private async sendOrderStatusMail(data: OrderStatusUpdateJob) {
+    const { orderId, status, email } = data;
+
+    await this.mailSenderService.sendOrderStatusUpdateMail(
+      email,
+      orderId,
+      status,
+    );
+
     console.log('Order status mail is sent.');
   }
 
-  private async passwordReset(job: Job) {
-    const { email, username, link } = job.data;
+  private async passwordReset(data: PasswordResetJob) {
+    const { email, username, link } = data;
     await this.mailSenderService.sendResetPasswordMail(email, username, link);
     console.log('Password reset mail is sent.');
+  }
+
+  private async sendRefundCreatedMail(data: OrderRefundJob) {
+    const { orderId, amount, email, customerName } = data;
+
+    await this.mailSenderService.sendRefundCreatedMail({
+      orderId,
+      amount,
+      email,
+      customerName,
+    });
+  }
+
+  private async sendRefundCompletedMail(data: OrderRefundJob) {
+    const { orderId, amount, email, customerName } = data;
+
+    await this.mailSenderService.sendRefundCompletedMail({
+      orderId,
+      amount,
+      email,
+      customerName,
+    });
+  }
+
+  private async sendRefundFailedMail(data: OrderRefundJob) {
+    const { orderId, amount, email, customerName, failureReason } = data;
+
+    await this.mailSenderService.sendRefundFailedMail({
+      email,
+      orderId,
+      amount,
+      customerName,
+      failureReason,
+    });
   }
 }
