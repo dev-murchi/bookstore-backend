d8b608cdd1a98a4cd6f806c1288b9afc9fee80d9
Murat ÇAY <murtcay@gmail.com>
2025-06-02 00:46:50 +0300

    add unit tests for lifecycle hooks onModuleInit and onModuleDestroy of PrismaService

--- src/prisma/prisma.service.spec.ts
+++ src/prisma/prisma.service.spec.ts
@@ -10,9 +10,22 @@ describe('PrismaService', () => {
     }).compile();
 
     service = module.get<PrismaService>(PrismaService);
+
+    service.$connect = jest.fn();
+    service.$disconnect = jest.fn();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should call $connect on onModuleInit', async () => {
+    await service.onModuleInit();
+    expect(service.$connect).toHaveBeenCalled();
+  });
+
+  it('should call $disconnect on onModuleDestroy', async () => {
+    await service.onModuleDestroy();
+    expect(service.$disconnect).toHaveBeenCalled();
   });
 });
